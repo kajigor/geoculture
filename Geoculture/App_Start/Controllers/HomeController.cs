6f57a65bbcd2111634e7a58882abaadd24421690
Aleksandr Samofalov <aleksander.samofalov@gmail.com>
2014-04-21 04:03:21 +0400

    Create asp.net project from @Aza407 files
    Example of partial refresh has been added (click on marker)

---
+++ Geoculture/App_Start/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Geoculture.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string RandomNumber()
+        {
+            try
+            {
+                int a = int.Parse(Request.Form["a"]);
+                int b = int.Parse(Request.Form["b"]);
+                int r = new Random().Next(a, b);
+                return r.ToString();
+            }
+            catch (Exception e)
+            {
+                return "Bad-bad-bad request!";
+            }
+        }
+
+    }
+}
